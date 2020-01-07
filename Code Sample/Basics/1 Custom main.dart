ce2ba9470b7f6b31f0f8007505dba77442805ec4
Amar Kumar <amrkrcse@gmail.com>
2020-01-07 19:18:13 +0530

    Create 1 Custom main.dart
    App with App Bar

---
+++ Code Sample/Basics/1 Custom main.dart
@@ -0,0 +1,26 @@
+//custom written
+
+import 'package:flutter/material.dart';
+
+/*void main(){
+  runApp(MyApp());
+}*/
+
+void main() => runApp(MyApp());
+
+//create a widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //throw UnimplementedError();
+    /*return MaterialApp(home: Text('Hello!!'),);*/
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is default text !'),
+      ),
+    );
+  }
+}
